ab6735430159847c1bb23b5859c4217bbd68f10d
danajlim <danalim0819@gmail.com>
2025-06-20 15:30:29 +0900

    initial commit

---
+++ src/test/java/com/studyhub/studyhub_backend_studies/StudyhubBackendStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyhub.studyhub_backend_studies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyhubBackendStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
